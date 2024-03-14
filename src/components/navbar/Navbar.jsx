66c6a80b6ac41c22a1e7aa4db1313dc17a168fc2
kimi-olivero <migueloliveropazos@gmail.com>
2024-03-14 14:59:55 +0000

    arreglo del navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
 import './navbar.css';
 import Logo from '../../assets/Marca.png';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { IoCloseSharp } from 'react-icons/io5';
 import { FaFacebookF } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
@@ -9,6 +9,7 @@ import { FaBarsStaggered } from 'react-icons/fa6';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState('inicio');
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -18,60 +19,33 @@ const Navbar = () => {
     setMenuOpen(false);
   };
 
-  useEffect(() => {
-    const handleLinkClick = () => {
-      // Cierra el menú cuando se hace clic en un enlace
-      setMenuOpen(false);
-    };
-
-    const links = document.querySelectorAll('.link-menu');
-    links.forEach((link) => {
-      link.addEventListener('click', handleLinkClick);
-    });
-
-    return () => {
-      links.forEach((link) => {
-        link.removeEventListener('click', handleLinkClick);
-      });
-    };
-  }, []);
-
-  useEffect(() => {
-    if (menuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, [menuOpen]);
-
+  const handleLinkClick = (linkId) => {
+    setActiveLink(linkId);
+    closeMenu();
+  };
 
   return (
     <nav className='Navbar fixed-top'>
       <Container>
         <div className='navbar-contenedor'>
           <div className='navbar-img d-none d-md-block'>
-          <a href='#inicio'><img src={Logo} alt='' /></a>
-           
+            <a href='#inicio'><img src={Logo} alt='' /></a>
           </div>
           <div className={`menu m-auto ${menuOpen ? 'active' : ''}`}>
             <div className='menu-box'>
-              <a href='#inicio' className='primer-enlace' onClick={closeMenu}>
+              <a href='#inicio' className={`primer-enlace ${activeLink === 'inicio' ? 'active' : ''}`} onClick={() => handleLinkClick('inicio')}>
                 Inicio
               </a>
-              <a href='#Espacio-Chimi' className='link-menu' onClick={closeMenu}>
+              <a href='#Espacio-Chimi' className={`link-menu ${activeLink === 'Espacio-Chimi' ? 'active' : ''}`} onClick={() => handleLinkClick('Espacio-Chimi')}>
                 Espacio Chimi
               </a>
-              <a href='#Vinos' className='link-menu' onClick={closeMenu}>
+              <a href='#Vinos' className={`link-menu ${activeLink === 'Vinos' ? 'active' : ''}`} onClick={() => handleLinkClick('Vinos')}>
                 Vinos
               </a>
-              <a href='#Comidas' className='link-menu' onClick={closeMenu}>
+              <a href='#Comidas' className={`link-menu ${activeLink === 'Comidas' ? 'active' : ''}`} onClick={() => handleLinkClick('Comidas')}>
                 Comidas
               </a>
-              <a href='#Contacto' className='link-menu' onClick={closeMenu}>
+              <a href='#Contacto' className={`link-menu ${activeLink === 'Contacto' ? 'active' : ''}`} onClick={() => handleLinkClick('Contacto')}>
                 Contacto
               </a>
             </div>
@@ -79,11 +53,11 @@ const Navbar = () => {
 
           <div className='icons'>
             <a href="https://www.facebook.com/profile.php?id=61556950231428&sk=about&locale=es_LA" target="_blank"><FaFacebookF className='icono' /></a>
-            <a href="https://www.instagram.com/chimichurrivalencia?igsh=MW04cHl0ZmpseWJhNw=="target="_blank"><FaInstagram className='icono' /></a>
+            <a href="https://www.instagram.com/chimichurrivalencia?igsh=MW04cHl0ZmpseWJhNw==" target="_blank"><FaInstagram className='icono' /></a>
           </div>
 
           <div className='navbar-img d-block d-md-none text-center'>
-          <a href='#inicio'><img src={Logo} alt='' /></a>
+            <a href='#inicio'><img src={Logo} alt='' /></a>
           </div>
 
           {menuOpen ? (
